e44cc87d8f53075f7ba5c29fe98f70a2b3b31458
Mohan Anandan <amohanse@gmail.com>
2014-09-16 21:31:41 +0300

    build file to copy war file

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,5 @@
 echo "This is my new build script"
 echo source dir : %DEPLOYMENT_SOURCE%
 echo target dir : %DEPLOYMENT_TARGET%
-
+call :ExecuteCmd copy "%DEPLOYMENT_SOURCE%\sample.war" "%DEPLOYMENT_TARGET%\bin\apache-tomcat-7.0.52\webapps\ROOT.war"
+echo war file copied
