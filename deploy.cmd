ae9beb081177becb0702a2ac34e19cfacc6e0614
Mohan Anandan <amohanse@gmail.com>
2014-09-17 21:35:09 +0300

    Build file updated -1

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,5 @@
 echo "This is my new build script"
 echo source dir : %DEPLOYMENT_SOURCE%
 echo target dir : %DEPLOYMENT_TARGET%
-call :ExecuteCmd copy "%DEPLOYMENT_SOURCE%\sample.war" "%DEPLOYMENT_TARGET%\bin\apache-tomcat-7.0.52\webapps\ROOT.war"
+xcopy "%DEPLOYMENT_SOURCE%\sample.war" "%DEPLOYMENT_TARGET%\bin\apache-tomcat-7.0.52\webapps\ROOT.war" /Y
 echo war file copied
